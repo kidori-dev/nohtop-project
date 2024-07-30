6a73823f0e8d2e96693af13b92ecb631384ee8a8
K-Hancock <shxogh123@gmail.com>
2024-07-30 23:14:04 +0900

    리스닝 온 포트 3000

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-시작하자 도리
-시작하자 도리2
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.listen(port, (req, res) => {
+    console.log('Listening on port ' + port)
+})
+
+app.get('/', (req, res) => {
+    console.log('Use routing /')
+})
